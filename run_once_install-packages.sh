389c430a65a99b9f0b1eb86548d679212c83c727
Dmytru <Dmytro_Kaminskyi@external.mckinsey.com>
2024-11-24 23:36:13 +0200

    add initial install script

---
+++ run_once_install-packages.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Ensure Homebrew is installed
+if ! command -v brew &> /dev/null; then
+  echo "Homebrew not found. Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Brewfile path
+BREWFILE="./Brewfile"
+
+# Install packages from Brewfile
+if [[ -f "$BREWFILE" ]]; then
+  echo "Installing packages from Brewfile..."
+  brew bundle --file="$BREWFILE"
+else
+  echo "Brewfile not found at $BREWFILE"
+fi
