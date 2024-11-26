7db2456b1bf66922f9233d232a577b15e2f23abd
Dmitriy Kaminskiy <dmitriy.kaminskiy.dev@gmail.com>
2024-11-26 14:28:09 +0200

    up

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,8 +1,2 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
-vim.api.nvim_create_user_command("Dashboard", function()
-  require("snacks").dashboard.open()
-end, { desc = "Open Snacks Dashboard" })
-
-vim.keymap.set("n", "<leader>D", ":Dashboard<CR>", { noremap = true, silent = true })
